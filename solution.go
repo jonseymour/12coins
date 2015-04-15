f5594d9a02b2edcd4df921f4a431b4fb7603bfcc
Jon Seymour <jon.seymour@gmail.com>
2015-04-15 14:46:54 +1000

    This is an example of a deliberately incomplete and broken solution which is intended as an example of the form of a working solution.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- solution.go
+++ solution.go
@@ -1,7 +1,47 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Decide returns the identity of the counterfeit coin and what the relative
 // weight of that coin is with respect to any other coin.
 func decide(scale Scale) (int, Weight) {
-	return -1, equal // always wrong
+	switch scale.Weigh([]int{0, 1, 2}, []int{9, 10, 11}) {
+	case light:
+		switch scale.Weigh([]int{0, 2}, []int{1, 4}) {
+		case light:
+			switch scale.Weigh([]int{0}, []int{4}) {
+			case light:
+				return 0, light
+			case equal:
+				return 2, light
+			case heavy:
+				panic(fmt.Errorf("incomplete case"))
+			}
+		case heavy:
+			panic(fmt.Errorf("incomplete case"))
+		case equal:
+			panic(fmt.Errorf("incomplete case"))
+		}
+	case heavy:
+		switch scale.Weigh([]int{0, 2}, []int{1, 4}) {
+		case light:
+			switch scale.Weigh([]int{0}, []int{4}) {
+			case light:
+				return 0, light
+			case equal:
+				return 1, heavy
+			case heavy:
+				panic(fmt.Errorf("incomplete case"))
+			}
+		case equal:
+			panic(fmt.Errorf("incomplete case"))
+		case heavy:
+			panic(fmt.Errorf("incomplete case"))
+		}
+	case equal:
+		panic(fmt.Errorf("incomplete case"))
+	}
+	return -1, equal
 }
