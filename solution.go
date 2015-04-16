5886260fc31a2ce3dd034ed3393b65c3357f6ec7
Jon Seymour <jon.seymour@gmail.com>
2015-04-16 10:17:32 +1000

    Introduce decide8 to exploit symmetry.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- solution.go
+++ solution.go
@@ -1,76 +1,39 @@
 package main
 
-import (
-	"fmt"
-)
+func decide8(scale Scale, weight1 Weight) (int, Weight) {
+	weight2 := scale.Weigh([]int{0, 5, 6}, []int{4, 1, 9})
+
+	if weight2 == equal {
+		weight3 := scale.Weigh([]int{2, 7}, []int{8, 9})
+		if weight3 == equal {
+			return 3, weight1
+		} else if weight3 == weight1 {
+			return 2, weight1
+		}
+		return 7, weight1.invert()
+	} else if weight2 == weight1 {
+		// # 0, 4
+		if scale.Weigh([]int{0}, []int{8}) == weight1 {
+			return 0, weight1
+		} else {
+			return 4, weight1.invert()
+		}
+	}
+	// 5, 6, 1
+	weight3 := scale.Weigh([]int{1, 5}, []int{8, 9})
+	if weight3 == equal {
+		return 6, weight1.invert()
+	} else if weight3 == weight1 {
+		return 1, weight1
+	}
+	return 5, weight1.invert()
+}
 
 // decide returns the identity of the different coin and what the relative
 // weight of that coin is with respect to any other coin.
 func decide(scale Scale) (int, Weight) {
-	switch scale.Weigh([]int{0, 1, 2, 3}, []int{4, 5, 6, 7}) {
-	case light:
-		switch scale.Weigh([]int{0, 5, 6}, []int{4, 1, 9}) {
-		case equal:
-			// 2, 3, 7
-			switch scale.Weigh([]int{2, 7}, []int{8, 9}) {
-			case equal:
-				return 3, light
-			case light:
-				return 2, light
-			case heavy:
-				return 7, heavy
-			}
-		case light:
-			// # 0, 4
-			if scale.Weigh([]int{0}, []int{8}) == light {
-				return 0, light
-			} else {
-				return 4, heavy
-			}
-		case heavy:
-			// 5, 6, 1
-			switch scale.Weigh([]int{1, 5}, []int{8, 9}) {
-			case equal:
-				return 6, heavy
-			case light:
-				return 1, light
-			case heavy:
-				return 5, heavy
-			}
-		}
-	case heavy:
-		switch scale.Weigh([]int{0, 5, 6}, []int{4, 1, 9}) {
-		case equal:
-			// 2, 3, 7
-			switch scale.Weigh([]int{2, 7}, []int{8, 9}) {
-			case equal:
-				return 3, heavy
-			case light:
-				return 7, light
-			case heavy:
-				return 2, heavy
-			}
-		case light:
-			// 5, 6, 1
-			switch scale.Weigh([]int{1, 5}, []int{8, 9}) {
-			case equal:
-				return 6, light
-			case light:
-				return 5, light
-			case heavy:
-				return 1, heavy
-			}
-		case heavy:
-			switch scale.Weigh([]int{0}, []int{8}) {
-			case equal:
-				return 4, light
-			case light:
-				panic(fmt.Errorf("cannot happen"))
-			case heavy:
-				return 0, heavy
-			}
-		}
-	case equal:
+	weight := scale.Weigh([]int{0, 1, 2, 3}, []int{4, 5, 6, 7})
+	if weight == equal {
 		switch scale.Weigh([]int{8, 9}, []int{10, 0}) {
 		case equal:
 			return 11, scale.Weigh([]int{11}, []int{0})
@@ -93,6 +56,8 @@ func decide(scale Scale) (int, Weight) {
 				return 10, light
 			}
 		}
+	} else {
+		return decide8(scale, weight)
 	}
 	return -1, equal
 }
