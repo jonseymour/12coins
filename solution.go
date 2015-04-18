288fa7be195e5878ec704c36dafee42703ed61f9
Jon Seymour <jon.seymour@gmail.com>
2015-04-18 14:16:19 +1000

    Add a decision table solution.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- solution.go
+++ solution.go
@@ -1,7 +1,122 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	coins    = 12
+	nextBits = 8
+	outcomes = 3
+	verbose  = false
+)
+
+var (
+	nextMask    = uint64((1 << nextBits) - 1)
+	weightShift = uint(8)
+	lightShift  = uint(64) - nextBits
+	equalShift  = lightShift - nextBits
+	heavyShift  = equalShift - nextBits
+	leftShift   = heavyShift - coins
+	rightShift  = leftShift - coins
+	coinsMask   = uint64((1 << coins) - 1)
+	coinMask    = uint64(0xff)
+	weightMask  = uint64(0x03)
+)
+
+var table = []uint64{
+	0x01190400F0F00000,
+	0x020B070611120000,
+	0x0300120113000000,
+	0x0000000000000000,
+	0x090D050611120000,
+	0x1100060113000000,
+	0x0000000000000200,
+	0x0816140223000000,
+	0x0000000000000001,
+	0x13150A0223000000,
+	0x0000000000000201,
+	0x0C0F180843000000,
+	0x0000000000000002,
+	0x17100E0843000000,
+	0x0000000000000202,
+	0x0000000000000003,
+	0x0000000000000203,
+	0x0000000000000004,
+	0x0000000000000204,
+	0x0000000000000005,
+	0x0000000000000205,
+	0x0000000000000006,
+	0x0000000000000206,
+	0x0000000000000007,
+	0x0000000000000207,
+	0x1A221C3004010000,
+	0x1B1E215000030000,
+	0x0000000000000008,
+	0x201F1D5000030000,
+	0x0000000000000208,
+	0x0000000000000009,
+	0x0000000000000209,
+	0x000000000000000A,
+	0x000000000000020A,
+	0x2300248000010000,
+	0x000000000000000B,
+	0x000000000000020B,
+}
+
+var index = make(map[uint64][]int)
+
+func nextShift(w Weight) uint {
+	return uint((heavy-w))*nextBits + heavyShift
+}
+
+func init() {
+	f := func(c uint64) []int {
+		b := c
+		a := []int{}
+		i := 0
+		for b != 0 {
+			if b&1 == 1 {
+				a = append(a, i)
+			}
+			i += 1
+			b >>= 1
+		}
+		return a
+	}
+
+	for _, e := range table {
+		c := (e >> leftShift) & coinsMask
+		if c != 0 {
+			if _, ok := index[c]; !ok {
+				index[c] = f(c)
+			}
+		}
+		d := (e >> rightShift) & coinsMask
+		if d != 0 {
+			if _, ok := index[d]; !ok {
+				index[d] = f(d)
+			}
+		}
+	}
+}
+
 // Decide returns the identity of the counterfeit coin and what the relative
 // weight of that coin is with respect to any other coin.
 func decide(scale Scale) (int, Weight) {
-	return -1, equal // always wrong
+
+	i := 0
+	for (table[i] >> heavyShift) != 0 {
+		r := table[i]
+		a := index[(r>>leftShift)&coinsMask]
+		b := index[(r>>rightShift)&coinsMask]
+		w := scale.Weigh(a, b)
+		p := (table[i] >> nextShift(w)) & nextMask
+		if verbose {
+			fmt.Fprintf(os.Stderr, "%d, %016x, %v, %v, %v, %d\n", i, r, a, b, w, p)
+		}
+		i = int(p)
+	}
+	return int(table[i] & coinMask), Weight((table[i] >> weightShift) & weightMask)
 }
