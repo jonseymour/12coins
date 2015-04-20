8997a971bdd00b003fa74965d5cb05af8c352788
Jon Seymour <jon.seymour@gmail.com>
2015-04-21 08:53:38 +1000

    Add permutation support to the library.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

---
+++ lib/permute.go
@@ -0,0 +1,40 @@
+package lib
+
+func Permute(origin []int) [][]int {
+
+	if len(origin) == 1 {
+		return [][]int{origin}
+	}
+	results := [][]int{}
+	for i, e := range origin {
+		c := make([]int, len(origin)-1, len(origin)-1)
+		copy(c, origin[0:i])
+		copy(c[i:], origin[i+1:])
+		for _, x := range Permute(c) {
+			p := append([]int{e}, x...)
+			results = append(results, p)
+		}
+	}
+	return results
+}
+
+type Permutation struct {
+	index []int
+}
+
+func NewPermutation(permutation []int) *Permutation {
+
+	index := make([]int, len(permutation), len(permutation))
+
+	for i, e := range permutation {
+		index[e] = i
+	}
+
+	return &Permutation{
+		index: index,
+	}
+}
+
+func (p *Permutation) Index(e int) int {
+	return p.index[e]
+}
