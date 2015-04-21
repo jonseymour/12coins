249dabdd4198dd06ef00bb2b42463dba9b67e22c
Jon Seymour <jon.seymour@gmail.com>
2015-04-21 16:25:07 +1000

    Produce a 1-based solver.

--- generator/generator.go
+++ generator/generator.go
@@ -13,16 +13,16 @@ const (
 
 var (
 	A = [2][]int{
-		[]int{2, 4, 0, 6}, []int{5, 7, 1, 3},
+		[]int{3, 5, 1, 7}, []int{6, 8, 2, 4},
 	}
 	B = [2][]int{
-		[]int{5, 10, 7, 0}, []int{8, 1, 6, 9},
+		[]int{6, 11, 8, 1}, []int{9, 2, 7, 10},
 	}
 	C = [2][]int{
-		[]int{2, 11, 7, 1}, []int{5, 8, 10, 4},
+		[]int{3, 12, 8, 2}, []int{6, 9, 11, 5},
 	}
 
-	IDENTITY = [12]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+	IDENTITY = [12]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 	WEIGHTS  = [12]lib.Weight{
 		light, heavy, light, heavy,
 		light, heavy, light, heavy,
@@ -35,19 +35,20 @@ func main() {
 		Weighings: [3][2][]int{A, B, C},
 		Coins:     IDENTITY,
 		Weights:   WEIGHTS,
+		ZeroCoin:  1,
 	}
 	for _, p := range lib.Permute([]int{0, 1, 2}) {
 		clone := ref.Clone()
 		clone.Permutation = p
 		clone.Weighings = [3][2][]int{clone.Weighings[p[0]], clone.Weighings[p[1]], clone.Weighings[p[2]]}
-		for i, _ := range IDENTITY {
-			o := lib.NewOracle(i, lib.Light)
+		for _, i := range IDENTITY {
+			o := lib.NewOracle(i, lib.Light, 1)
 			ri, rw := clone.Decide(o)
 			if ri != i {
-				clone.Coins[ri] = i
+				clone.Coins[ri-1] = i
 			}
 			if rw != lib.Light {
-				clone.Weights[ri] = lib.Heavy - clone.Weights[ri]
+				clone.Weights[ri-1] = lib.Heavy - clone.Weights[ri-1]
 			}
 		}
 		clone.Relabel()
