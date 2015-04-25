554c070964c0ee686f3a6268f7c5c9794cb49a87
Jon Seymour <jon.seymour@gmail.com>
2015-04-25 22:23:35 +1000

    Add Union, Intersection, Add methods to CoinSet interface.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- lib/coinset.go
+++ lib/coinset.go
@@ -1,20 +1,25 @@
 package lib
 
+type CoinMask uint16
+
 type CoinSet interface {
 	AsCoins(zeroCoin int) []int
 	Size() uint8
 	Sort() CoinSet
+	Union(other CoinSet) CoinSet
+	Intersection(other CoinSet) CoinSet
+	Add(coin int, zeroCoin int) CoinSet
 }
 
 type coinset struct {
-	mask uint16
+	mask CoinMask
 	size uint8
 }
 
 func (s *coinset) AsCoins(zeroCoin int) []int {
 	result := make([]int, s.Size())
 	bits := s.mask
-	mask := uint16(1)
+	mask := CoinMask(1)
 	coin := 0
 	i := uint8(0)
 	for i < s.size {
@@ -36,8 +41,35 @@ func (s *coinset) Sort() CoinSet {
 	return s
 }
 
+func (s *coinset) Union(other CoinSet) CoinSet {
+	var o *coinset
+	var ok bool
+	if o, ok = other.(*coinset); !ok {
+		o = NewCoinSet(other.AsCoins(0), 0).(*coinset)
+	}
+	return NewCoinSetFromMask(s.mask | o.mask)
+}
+
+func (s *coinset) Intersection(other CoinSet) CoinSet {
+	var o *coinset
+	var ok bool
+	if o, ok = other.(*coinset); !ok {
+		o = NewCoinSet(other.AsCoins(0), 0).(*coinset)
+	}
+	return NewCoinSetFromMask(s.mask & o.mask)
+}
+
+func (s *coinset) Add(coin int, zeroCoin int) CoinSet {
+	mask := CoinMask(1 << uint(coin-zeroCoin))
+	if (s.mask & mask) == 0 {
+		return NewCoinSetFromMask(s.mask | mask)
+	} else {
+		return s
+	}
+}
+
 func NewCoinSet(coins []int, zeroCoin int) CoinSet {
-	mask := uint16(0)
+	mask := CoinMask(0)
 	count := uint8(0)
 	for _, e := range coins {
 		nextmask := mask | (1 << uint(e-zeroCoin))
@@ -45,6 +77,23 @@ func NewCoinSet(coins []int, zeroCoin int) CoinSet {
 			mask = nextmask
 			count += 1
 		}
+	}
+	return &coinset{
+		mask: mask,
+		size: count,
+	}
+}
+
+func NewCoinSetFromMask(mask CoinMask) CoinSet {
+	newMask := CoinMask(0)
+	bit := CoinMask(1)
+	count := uint8(0)
+	for newMask != mask {
+		if bit&mask != 0 {
+			newMask |= bit
+			count += 1
+		}
+		bit <<= 1
 	}
 	return &coinset{
 		mask: mask,
