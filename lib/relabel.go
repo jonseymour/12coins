f4a783bc71d27bd2871cc0c3467f927f0cd50547
Jon Seymour <jon.seymour@gmail.com>
2015-04-26 14:06:07 +1000

    Separate relabeling concern into a separate module.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

---
+++ lib/relabel.go
@@ -0,0 +1,51 @@
+package lib
+
+import (
+	"sort"
+)
+
+// Relabel the coins of the weighing such that the Coins
+// slice is numbered in strictly increasing numerical order.
+func (s *Solver) Relabel() (*Solver, error) {
+
+	var clone *Solver
+	var err error
+
+	if len(s.Coins) != 12 {
+		if clone, err = s.Reverse(); err != nil {
+			s.Valid = pbool(false)
+			return s, err
+		}
+	} else {
+		clone = s.Clone()
+	}
+
+	clone.resetCounts()
+
+	c := make([]int, len(clone.Coins), len(clone.Coins))
+	for i, e := range clone.Coins {
+		c[i] = e
+	}
+	p := NewPermutation(c, clone.ZeroCoin)
+
+	for i, w := range clone.Weighings {
+		coinSet := [2]CoinSet{}
+		for j, pan := range w.Pans() {
+			coins := pan.AsCoins(clone.ZeroCoin)
+			for k, e := range coins {
+				coins[k] = p.Index(e) + clone.ZeroCoin
+			}
+			sort.Sort(sort.IntSlice(coins))
+			coinSet[j] = NewCoinSet(coins, clone.ZeroCoin)
+		}
+		clone.Weighings[i] = NewWeighing(coinSet[0], coinSet[1])
+	}
+
+	for i, _ := range clone.Coins {
+		clone.Coins[i] = i + clone.ZeroCoin
+	}
+
+	clone.flags |= (RELABLED | NORMALISED) &^ (CANONICALISED | GROUPED | ANALYSED)
+
+	return clone, nil
+}
