e629107af7ea163869b8115017e6b6890aa5cd68
Jon Seymour <jon.seymour@gmail.com>
2015-04-26 13:28:20 +1000

    Add --structure and --canonical switches to command line tool.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- tools/tools.go
+++ tools/tools.go
@@ -14,12 +14,16 @@ func main() {
 	normalize := false
 	groupings := false
 	valid := false
+	structure := false
+	canonical := false
 
 	flag.BoolVar(&reverse, "reverse", false, "Derive the coins and weights array from the weighings.")
 	flag.BoolVar(&relabel, "relabel", false, "Relabel solution into a indexing form.")
 	flag.BoolVar(&normalize, "normalize", false, "Order the coins in each weighing from lowest to highest.")
 	flag.BoolVar(&groupings, "groupings", false, "Derive the singletons, pairs and triples from the weighings.")
 	flag.BoolVar(&valid, "valid", false, "Only pass valid solutions to stdout.")
+	flag.BoolVar(&structure, "structure", false, "Analyse the structure of the weighings.")
+	flag.BoolVar(&canonical, "canonical", false, "Permute the weighings into the canonical form.")
 	flag.Parse()
 
 	decoder := json.NewDecoder(os.Stdin)
@@ -60,6 +64,20 @@ func main() {
 			if solver, err = solver.Groupings(); err != nil {
 				ok = false
 				fmt.Fprintf(os.Stderr, "error: groupings: %v: %v\n", err, solver)
+			}
+		}
+
+		if structure && ok {
+			if solver, err = solver.AnalyseStructure(); err != nil {
+				ok = false
+				fmt.Fprintf(os.Stderr, "error: structure: %v: %v\n", err, solver)
+			}
+		}
+
+		if canonical && ok {
+			if solver, err = solver.Canonical(); err != nil {
+				ok = false
+				fmt.Fprintf(os.Stderr, "error: canonical: %v: %v\n", err, solver)
 			}
 		}
 
