2c6dc3b2686c39920509c8a1295718f0da94d4a4
Jon Seymour <jon.seymour@gmail.com>
2015-04-26 14:09:22 +1000

    Separate grouping concerns into a separate module.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- lib/solver.go
+++ lib/solver.go
@@ -159,47 +159,6 @@ func pbool(b bool) *bool {
 	return &b
 }
 
-func (s *Solver) resetCounts() {
-	s.Unique = nil
-	s.Triples = nil
-	s.Pairs = [3]CoinSet{nil, nil, nil}
-}
-
-// Tabulate the singletons, pairs and triples of the solution.
-func (s *Solver) Groupings() (*Solver, error) {
-	clone := s.Clone()
-	clone.resetCounts()
-
-	a := clone.Weighings[0].Both()
-	b := clone.Weighings[1].Both()
-	c := clone.Weighings[2].Both()
-
-	ab := a.Intersection(b)
-	bc := b.Intersection(c)
-	ca := c.Intersection(a)
-
-	all := a.Union(b).Union(c)
-	triples := ab.Intersection(bc).Intersection(ca)
-	singletons := a.Complement(b.Union(c)).Union(b.Complement(a.Union(c))).Union(c.Complement(a.Union(b)))
-	pairs := all.Complement(triples).Complement(singletons)
-
-	if triples.Size() != 3 || singletons.Size() != 3 || pairs.Size() != 6 {
-		s.Valid = pbool(false)
-		return s, fmt.Errorf("invalid grouping sizes: %v, %v, %v", triples, pairs, singletons)
-	}
-
-	abp := pairs.Intersection(ab)
-	bcp := pairs.Intersection(bc)
-	cap := pairs.Intersection(ca)
-
-	clone.Triples = triples
-	clone.Unique = singletons
-	clone.Pairs = [3]CoinSet{abp, bcp, cap}
-	clone.flags |= GROUPED
-
-	return clone, nil
-}
-
 // Return a clone of the receiver in which the structure has been populated.
 func (s *Solver) AnalyseStructure() (*Solver, error) {
 	var r *Solver
